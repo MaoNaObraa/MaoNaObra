a690c7bdb637932ca0041986b9ba33ab34d1cef2
Wendel Isaac <58012282+wendelisc12@users.noreply.github.com>
2023-07-14 21:03:28 +0000

    feat: Forgot password commit

---
+++ src/pages/ForgotPassword/ForgotPassword.jsx
@@ -0,0 +1,11 @@
+import './ForgotPassword.css'
+
+const ForgotPassword = () => {
+    return ( 
+        <>
+            <h1>faz tudo aqui</h1>
+        </>
+    );
+}
+ 
+export default ForgotPassword;
