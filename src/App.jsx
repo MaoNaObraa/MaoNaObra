b28e7c0421a95b6149574be6b213e198cda5f7d4
Wendel <wendel.isaac12@gmail.com>
2023-07-14 17:53:27 -0300

    feat: Registration screen

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import './App.css'
 import Navigation from './components/navigation/Navigation';
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Home from './pages/Home/Home';
+import Cadastro from './pages/Cadastro/Cadastro';
 
 function App() {
   return (
     <>
 
-        <Navigation />
+      <Cadastro />
+
+        {/* <Navigation />
           <BrowserRouter>
 
             <Switch>
@@ -17,7 +20,7 @@ function App() {
               </Route> 
                 
             </Switch>
-          </BrowserRouter>
+          </BrowserRouter> */}
     </>
   )
 }
---
+++ src/pages/Cadastro/Cadastro.jsx
@@ -0,0 +1,11 @@
+import './cadastro.css'
+
+const Cadastro = () => {
+    return ( 
+        <>
+            <h1>faz tudo aqui</h1>
+        </>
+    );
+}
+ 
+export default Cadastro;
