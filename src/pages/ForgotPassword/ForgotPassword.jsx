95133c89f054ce36725a2c6bd3653b8616a77220
Wendel Isaac <58012282+wendelisc12@users.noreply.github.com>
2023-07-19 10:33:05 -0300

    Update ForgotPassword.jsx

--- src/pages/ForgotPassword/ForgotPassword.jsx
+++ src/pages/ForgotPassword/ForgotPassword.jsx
@@ -3,7 +3,25 @@ import './ForgotPassword.css'
 const ForgotPassword = () => {
     return ( 
         <>
-            <h1>faz tudo aqui</h1>
+            <div class="head">
+            <img src="./logo.png" alt="" class="icon">
+            <div class="text">MãoNaObra</div>
+        </div>
+        <div class="caixa"> 
+            <div class="caixa-titulo"> 
+                Esqueceu sua senha?
+            </div>
+            <div class="caixa-subtitulo">
+                Digite seu endereço de e-mail para redefinir sua senha.
+            </div>
+            <div class="caixa-input">
+                <input type="email" id="email" placeholder="E-mail"/>
+            </div>
+            <div class="botao">
+                Recuperar senha
+
+            </div>
+        </div>
         </>
     );
 }
