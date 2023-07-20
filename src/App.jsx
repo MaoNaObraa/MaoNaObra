515c1fd0e4a35e14cde0c9e2d70f0bd7237bcbe4
Wendel <wendel.isaac12@gmail.com>
2023-07-19 21:20:37 -0300

    feature: creation component Prestador de serviço

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import Navigation from './components/navigation/Navigation';
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Home from './pages/Home/Home';
 import Login from './pages/Login/login'
+import PrestadorServico from './components/prestadorServico/PrestadorServico';
 
 function App() {
+
   return (
     <>
+      
+      <PrestadorServico nome="kemysson"/>
 
-    <Login />
+      {/* <Login /> */}
 
         {/* <Navigation />
           <BrowserRouter>
---
+++ src/components/prestadorServico/PrestadorServico.jsx
@@ -0,0 +1,11 @@
+import './PrestadorServico.css'
+
+const PrestadorServico = ({nome}) => {
+    return ( 
+        <>
+            <h1>{nome}</h1>
+        </>
+     );
+}
+ 
+export default PrestadorServico;
