6a620bf95db66c5d94610339ef1a5b6ee2996466
Wendel <wendel.isaac12@gmail.com>
2023-07-20 01:34:07 -0300

    feat: component categories

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import Navigation from './components/navigation/Navigation';
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Home from './pages/Home/Home';
 import Login from './pages/Login/login'
+import Categories from './components/categories/Categories';
 
 function App() {
   return (
     <>
 
-    <Login />
+      <Categories />
 
         {/* <Navigation />
           <BrowserRouter>
