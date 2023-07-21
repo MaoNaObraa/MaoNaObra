e36fc6aa760b8a04bd19b09ff3a26d8d9ca55a80
Wendel <wendel.isaac12@gmail.com>
2023-07-21 13:20:43 -0300

    fix: component comment

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -1,37 +1,25 @@
 import './Comment.css'
 
-const Comment = ({Nome,Avaliacao,Comentario}) => {
-    return ( 
-        <main class="d-flex justify-content-center align-items-center">
+const Comment = ({ Nome, Avaliacao, Comentario }) => {
+    return (
+            <div className="d-flex border align-items-center" id='comentario'>
 
-            
-        
-        
-           
-            
-                <div className="d-flex border align-items-center" id='comentario'>
-                    
-                        <div id="foto-comentario-box" className=''>
-                           <div id="foto-comentario">
-                             
-                           </div>
-                                
-                        </div>   
-                            <div className='d-flex flex-column'>
-                                <h4>{Nome}</h4>
-                                <h5>{Avaliacao}</h5>
+                <div id="foto-comentario-box" className=''>
+                    <div id="foto-comentario">
 
-                                <p>
-                                   {Comentario}
-                                </p>
-                            </div>
-                    
-
+                    </div>
 
                 </div>
-           
-        </main>
-     );
+                <div className='d-flex flex-column'>
+                    <h4>{Nome}</h4>
+                    <h5>{Avaliacao}</h5>
+
+                    <p>
+                        {Comentario}
+                    </p>
+                </div>
+            </div>
+    );
 }
- 
+
 export default Comment;
