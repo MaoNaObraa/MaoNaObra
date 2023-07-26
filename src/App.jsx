1eccf0c800acb121d293803067eba6f3a03cb3de
Wendel <wendel.isaac12@gmail.com>
2023-07-26 14:23:43 -0300

    fix: forgot password screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,10 @@
 import './App.css'
-import Navigation from './components/navigation/Navigation';
-import {BrowserRouter, Switch, Route} from "react-router-dom";
-import Home from './pages/Home/Home';
 import ForgotPassword from './pages/ForgotPassword/ForgotPassword'
 
 function App() {
   return (
     <>
-
     <ForgotPassword />
-{/* 
-        <Navigation />
-          <BrowserRouter>
-
-            <Switch>
-
-              <Route path='/'exact>
-                <Home />
-              </Route> 
-                
-            </Switch>
-          </BrowserRouter> */}
     </>
   )
 }
