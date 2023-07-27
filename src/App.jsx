d2072eca90d110bd3e302275e60c89fa86d1d3cd
Wendel <wendel.isaac12@gmail.com>
2023-07-27 03:08:49 -0300

    fix: css fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,16 @@
-import './App.css'
 import Navigation from './components/navigation/Navigation';
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Home from './pages/Home/Home';
 import Login from './pages/Login/login'
 import PrestadorServico from './components/prestadorServico/PrestadorServico';
+import foto from './components/prestadorServico/foto.png'
 
 function App() {
 
   return (
     <>
+    <PrestadorServico foto={foto} nome="Kemysson" descricao="Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed efficitur ante. Praesent iaculis laoreet justo, et vestibulum ipsum consectetur." servicos={["pedreiro", "encanador", "encanador", "encanador", "encanador"]} avaliacao="5.0" />
       
-
-       <Login /> 
-
-        {/* <Navigation />
-          <BrowserRouter>
-
-            <Switch>
-
-              <Route path='/'exact>
-                <Home />
-              </Route> 
-                
-            </Switch>
-          </BrowserRouter> */}
     </>
   )
 }
