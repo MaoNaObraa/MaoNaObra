6d4e7cd6858a0892a1e2fce4850af79c4db23c0f
Wendel <wendel.isaac12@gmail.com>
2023-07-28 16:02:08 -0300

    feat: organization of cards components

---
+++ src/components/Home-components/cards-PrestadorServico/CardsPrestadorServico.jsx
@@ -0,0 +1,17 @@
+import PrestadorServico from '../../prestadorServico/PrestadorServico'
+import fotoExemple from '/foto.png'
+
+const CardsPrestadorServico = () => {
+    return (
+        <>
+        <h4>Em destaque</h4>
+        <div className="d-flex justify-content-between">
+            <PrestadorServico foto={fotoExemple} nome="Larissa Soares" descricao="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed efficitur ante. Praesent iaculis laoreet justo, et vestibulum ipsum consectetur." servicos={["encanador"]} avaliacao="4.5" />
+            <PrestadorServico foto={fotoExemple} nome="Alef Aquino" descricao="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed efficitur ante. Praesent iaculis laoreet justo, et vestibulum ipsum consectetur." servicos={["Pedreiro", "eletricista", "encanador"]} avaliacao="4.5" />
+            <PrestadorServico foto={fotoExemple} nome="Kemysson" descricao="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed efficitur ante. Praesent iaculis laoreet justo, et vestibulum ipsum consectetur." servicos={["Jardineiro", "pintor"]} avaliacao="4.5" />
+        </div>
+        </>
+    );
+}
+
+export default CardsPrestadorServico;
