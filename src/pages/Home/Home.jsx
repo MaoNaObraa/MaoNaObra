b8536dd36fbd4b7afcf7e1447efd656deee6362d
Wendel Isaac <58012282+wendelisc12@users.noreply.github.com>
2023-07-27 21:07:22 -0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,37 +1,30 @@
+import CardsPrestadorServico from '../../components/cards-PrestadorServico/CardsPrestadorServico';
+import Categories from '../../components/categories-filter/categories/Categories';
+import FilterSearch from '../../components/categories-filter/filter/FilterSearch';
 import './Home.css'
 
 const Home = () => {
     return ( 
-        <div className=' d-flex  justify-content-between' id='home-box'>
-          <div id='informations' className='d-flex flex-column  align-items-center'>
-            <div id='img' className='d-flex mt-5'>
+        <div className='container d-flex justify-content-between' id='home-box'>
+            <div id='categories-box'>
+                <FilterSearch />
+                <Categories />
             </div>
-               <h3 className='d-flex' id='nome'>Laura Nogueira</h3>
-                <h5>28 anos</h5>
-                  <h5>Eletricista</h5>
-          </div>
-          <div id='description' className='d-flex  m-5 flex-column'>
-            <div id='alinhar'className='d-flex flex-column m-5 p-2 align-itemns-center'> 
-             <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus libero felis, lacinia dictum mauris vitae,<br/> 
-               laoreet dictum est. Proin vestibulum elementum lectus, ut lobortis est luctus ut. Nunc et quam ex. Sed<br/> 
-               interdum, nulla at ultrices porttitor, diam felis finibus erat, vel ullamcorper tortor arcu eu ex. In ultrices<br/> 
-               purus vitae ullamcorper porta. Aenean porta, ligula nec sodales sodales, lorem nulla efficitur ante, sit<br/> 
-               amet bibendum lacus elit ac elit. Cras nec facilisis magna, non pellentesque est. Proin nisl sapien,<br/> 
-               pulvinar vel vulputate sed, fermentum eu enim. Curabitur fermentum dui non luctus imperdiet.
-             </h5>
-               <div className='d-flex p-3 mt-3'>
-               <button id='botao-contratar'> Contratar </button>
-               </div>
-               <div className='d-flex p-5 flex-column'> 
-                <h4 id='avaliacao'>23 avaliações</h4>
-                <h4 id='estrela'>Estrelasssss</h4>
-                <h4 id='comentario'>comentários (10)</h4>
-               </div>
-             
-           </div>   
-          </div>
+
+            <div id='principal-box-home' className='pb-3'>
+                <div className='d-flex' id='pesquisar-box'>
+                    <input type="text" placeholder='Pesquise um serviço de seu interesse' id='pesquisar-input'/>
+                    <button id='pesquisar-button'>Pesquisar</button>
+                </div>
+                <div className='mt-4' id="persons">
+                    <h4 className='mb-4'>Em destaque</h4>
+                    <div>
+                        <CardsPrestadorServico />
+                    </div>
+                </div>
+            </div>
         </div>
      );
 }
  
-export default Home;  
+export default Home;
