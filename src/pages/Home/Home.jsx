51316ed436b5b108da3bba9bd55199ebcf0f3d25
Wendel <wendel.isaac12@gmail.com>
2023-07-28 16:01:47 -0300

    feat: show results of search

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,29 @@
-import { useState } from 'react';
-import CardsPrestadorServico from '../../components/cards-PrestadorServico/CardsPrestadorServico';
+import { useEffect, useState } from 'react';
+import CardsPrestadorServico from '../../components/Home-components/cards-PrestadorServico/CardsPrestadorServico';
 import Categories from '../../components/categories-filter/categories/Categories';
 import FilterSearch from '../../components/categories-filter/filter/FilterSearch';
 import './Home.css'
+import SearchResults from '../../components/Home-components/searchResults/searchResults';
 
 const Home = () => {
     // recuperando os dados da barra de pesquisa
-    const[pesquisaValue, setPesquisaValue] = useState('')
-    const pesquisar = () =>{
-        console.log(pesquisaValue)
+    const [pesquisaValue, setPesquisaValue] = useState('')
+    //mostrar resultado da pesquisa
+    const [mostrarResultados, setMostrarResultados] = useState(false)
+
+    // função para retornar o que foi pesquisado
+    const pesquisar = () => {
+        setMostrarResultados(true)
     }
 
-    return ( 
+    // caso a barra de pesquisa esteja vazia, voltar para componente de cards
+    useEffect(()=>{
+        if(pesquisaValue == ""){
+            setMostrarResultados(false)
+        }
+    }, [pesquisaValue])
+
+    return (
         <div className='container d-flex justify-content-between' id='home-box'>
             <div id='categories-box'>
                 <FilterSearch />
@@ -20,18 +32,19 @@ const Home = () => {
 
             <div id='principal-box-home' className='pb-3'>
                 <div className='d-flex' id='pesquisar-box'>
-                    <input type="text" placeholder='Pesquise um serviço de seu interesse' id='pesquisar-input' value={pesquisaValue} onChange={(e)=>{ setPesquisaValue(e.target.value)}}/>
+                    <input type="text" placeholder='Pesquise um serviço de seu interesse' id='pesquisar-input' value={pesquisaValue} onChange={(e) => { setPesquisaValue(e.target.value) }} />
                     <button id='pesquisar-button' onClick={pesquisar}>Pesquisar</button>
                 </div>
-                <div className='mt-4' id="persons">
-                    <h4 className='mb-4'>Em destaque</h4>
+                <div className='mt-4 mb-4' id="persons">
                     <div>
-                        <CardsPrestadorServico />
+                        {/* se o estado mostrarResultado for falso, então mostra os cards normalmente, se for verdadeiro retornar o componente de resultado de pesquisa */}
+                        {mostrarResultados && <SearchResults textoPesquisa={pesquisaValue}/>}
+                        {!mostrarResultados && <CardsPrestadorServico />}
                     </div>
                 </div>
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default Home;
