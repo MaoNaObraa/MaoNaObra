e85eabd77aef91343e1e45b0e7fb44ab473ce915
João <jgbarbosac32@gmail.com>
2023-08-01 03:31:57 -0300

    forms feat: falta a mensagem da data de nascimento

--- src/components/Cadastro-components/dados-pessoais/DadosPessoais.jsx
+++ src/components/Cadastro-components/dados-pessoais/DadosPessoais.jsx
@@ -1,13 +1,33 @@
 import Input from "../../input/Input";
 import './DadosPessoais.css'
-import PreviousNextButtons from "../previous-next/PreviousNextButtons";
 import { useForm } from "react-hook-form"
 import { yupResolver } from "@hookform/resolvers/yup"
 import * as yup from "yup"
+import { useHistory } from 'react-router-dom';
 
+yup.setLocale({
+        date: {
+            max: 'Data deve ser anterior ou igual a ${max}',
+          },
+     
+
+})
 const validationSchema = yup.object().shape({
-    cpf: yup.string().required("Campo obrigatório."),
-  })
+      cpf: yup.string().required('Campo obrigatório.').matches(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/, 'CPF inválido. O formato correto é XXX.XXX.XXX-XX.'),
+      rg: yup.string().required('Campo obrigatório.').matches(/^\d{2}\.\d{3}\.\d{3}$/, 'RG inválido. O RG deve ter o formato XX.XXX.XXX.'),
+      dataNascimento: yup
+    .date()
+    .max(new Date(), '${path} não pode ser no futuro')
+    .required('${path} é um campo obrigatório'),
+      endereco: yup.string().required(),
+      numero: yup.string().required('Campo obrigatório.').matches(/^\d+$/, 'Este campo deve conter apenas números.'),
+      cep: yup.string().required('Campo obrigatório.').matches(/^\d{5}\-\d{3}$/, 'CEP inválido. O formato correto é XXXXX-XXX.'),
+      estado: yup.string().required('Campo obrigatório.'),
+      bairro: yup.string().required('Campo obrigatório.'),
+      cidade: yup.string().required('Campo obrigatório.'),
+      complemento: yup.string().required('Campo obrigatório.')
+      });
+
 
 const DadosPessoais = () => {
 
@@ -17,7 +37,25 @@ const DadosPessoais = () => {
         resolver: yupResolver(validationSchema)
       });
       
-      const addPost = Data => { console.log(Data) }
+      const addPost = (data) => {
+        console.log(data);
+        if (Object.keys(errors).length === 0) { 
+            enviarSuasInformacoes(); 
+        } else {
+            console.log("Existem erros no formulário.");
+        }
+    }
+
+    const history = useHistory();
+    
+   function voltarParaSuasInformacoes(){
+    history.push("/cadastro/suasInformacoes")
+   }
+
+    function enviarSuasInformacoes() {
+        // Redirecionar para a próxima página após a submissão do formulário
+        history.push('/cadastro/TipoCadastro');
+    }
 
     return (
         <>
@@ -32,36 +70,41 @@ const DadosPessoais = () => {
                         <form action="" onSubmit={handleSubmit(addPost)}>
                             <div className='d-flex w-100 justify-content-between'>
                                 <div style={{ width: "49%" }}><Input id="cpf" label="CPF" type="text" name="cpf" placeholder="Digite seu cpf" validation={{ control }}  error={errors.cpf} /></div>
-                                {/* <div style={{ width: "49%" }}><Input id="rg" label="RG" type="text" nome="rg" placeholder="Digite seu RG" /></div> */}
+                                 <div style={{ width: "49%" }}><Input id="rg" label="RG" type="text" name="rg" placeholder="Digite seu RG"  validation={{ control }}  error={errors.rg}/></div> 
                             </div>
-                            {/* <Input id="dataNascimento" label="Data de nascimento" type="date" name="dataNascimento" placeholder="Digite sua data de nascimento" /> */}
-                        </form>
+                              <Input id="dataNascimento" label="Data de nascimento" type="date" name="dataNascimento" placeholder="Digite sua data de nascimento" validation={{ control }}  error={errors.dataNascimento} /> 
+                             </form>
                     </div>
                 </div>
-                {/* <div className="mt-2">
+                  <div className="mt-2">
                     <div className='first-text-cadastro mb-3'>
                         <h2 className='text-principal'>Endereço</h2>
                         <p>Insira seu endereço para melhor segurança</p>
                     </div>
+                    <form action="" onSubmit={handleSubmit(addPost)}>
                     <div className='d-flex w-100 justify-content-between'>
-                        <div style={{ width: "80%" }}><Input id="endereco" label="Endereço" type="text" nome="endereco" placeholder="Digite seu endereço" value="" onchange="" /></div>
-                        <div style={{ width: "19%" }}><Input id="numero" label="Número" type="text" nome="numero" placeholder="Digite o número" value="" onchange="" /></div>
+                        <div style={{ width: "80%" }}><Input id="endereco" label="Endereço" type="text" name="endereco" placeholder="Digite seu endereço" validation={{ control }}  error={errors.endereco}  /></div>
+                       <div style={{ width: "19%" }}><Input id="numero" label="Número" type="text" name="numero" placeholder="Digite o número" validation={{ control }}  error={errors.numero}/></div>
+                    </div>
+                     <div className='d-flex w-100 justify-content-between'>
+                         <div style={{ width: "49%" }}><Input id="cep" label="CEP" type="text" name="cep" placeholder="Digite seu CEP" validation={{ control }}  error={errors.cep}/></div> 
+                        <div style={{ width: "49%" }}><Input id="estado" label="Estado" type="text" name="estado" placeholder="Digite o seu estado" validation={{ control }}  error={errors.estado} /></div>
                     </div>
                     <div className='d-flex w-100 justify-content-between'>
-                        <div style={{ width: "49%" }}><Input id="cep" label="CEP" type="text" nome="cep" placeholder="Digite seu CEP" value="" onchange="" /></div>
-                        <div style={{ width: "49%" }}><Input id="estado" label="Estado" type="text" nome="estado" placeholder="Digite o seu estado" value="" onchange="" /></div>
+                         <div style={{ width: "49%" }}><Input id="bairro" label="Bairro" type="text" name="bairro" placeholder="Digite seu bairro" validation={{ control }}  error={errors.bairro} /></div>
+                        <div style={{ width: "49%" }}><Input id="cidade" label="Cidade" type="text" name="cidade" placeholder="Digite a sua cidade"  validation={{ control }}  error={errors.cidade} /></div> 
                     </div>
-                    <div className='d-flex w-100 justify-content-between'>
-                        <div style={{ width: "49%" }}><Input id="bairro" label="Bairro" type="text" nome="bairro" placeholder="Digite seu bairro" value="" onchange="" /></div>
-                        <div style={{ width: "49%" }}><Input id="cidade" label="Cidade" type="text" nome="cidade" placeholder="Digite a sua cidade" value="" onchange="" /></div>
-                    </div>
-                    <Input id="complemento" label="Complemento" type="text" nome="complemento" placeholder="Digite o complemento" value="" onchange="" />
-                </div> */}
+                     <Input id="complemento" label="Complemento" type="text" name="complemento" placeholder="Digite o complemento" validation={{ control }}  error={errors.complemento}/> 
+                    
+
 
                 <div className='mt-4 d-flex align-items-center justify-content-between'>
-                    <PreviousNextButtons link="/cadastro/suasInformacoes" />
-                    <PreviousNextButtons text="Proxima etapa" link="/cadastro/tipoCadastro" type="next" />
+                    <button  className='rounded mt-2' id='buttonVoltarDadosPessoais'onClick={voltarParaSuasInformacoes}> Voltar</button>
+                  <button id='button-suasInfos'  type='button' onClick={handleSubmit(addPost)} className='rounded text-light mt-2'>Próxima etapa</button>
                 </div>
+                </form>
+                </div>  
+                <div></div>
             </div>
         </>
     );
