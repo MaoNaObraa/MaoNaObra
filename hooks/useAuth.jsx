846c4872858235c463d1197b1d5eb2505e369441
Wendel <wendel.isaac12@gmail.com>
2023-08-03 01:17:43 -0300

    feat: creating conexion api to register user

---
+++ hooks/useAuth.jsx
@@ -0,0 +1,22 @@
+import api from "../utils/api";
+import {useState, useEffect} from "react"
+import {useHistory} from "react-router-dom"
+
+const useAuth = () => {
+    async function register(user){
+
+        try {
+            const data = await api.post('/users/register', user).then((response)=>{
+                return response.data
+            })
+
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return {register}
+}
+ 
+export default useAuth;
