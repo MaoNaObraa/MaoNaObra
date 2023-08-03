dcd5d1a2525c60e4730c07cbc5f725044ea3dfc6
Wendel <wendel.isaac12@gmail.com>
2023-08-03 15:47:17 -0300

    feat: function to register, login and logout user

--- hooks/useAuth.jsx
+++ hooks/useAuth.jsx
@@ -3,6 +3,31 @@ import {useState, useEffect} from "react"
 import {useHistory} from "react-router-dom"
 
 const useAuth = () => {
+    const[authenticated, setAuthenticated] = useState(false)
+    const history = useHistory()
+
+    useEffect(()=>{
+        const token = localStorage.getItem('token')
+
+        if(token){
+            api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`
+            setAuthenticated(true)
+        }
+    }, [])
+
+    async function login(user){
+        try {
+            const data = await api.post('/users/login', user).then((response)=>{
+                return response.data
+            })
+
+            await authUser(data)
+            history.push('/')
+        } catch (error) {
+            console.log(error.response)
+        }
+    }
+
     async function register(user){
 
         try {
@@ -10,13 +35,27 @@ const useAuth = () => {
                 return response.data
             })
 
-            console.log(data)
+            await authUser(data)
+
         } catch (error) {
             console.log(error)
         }
     }
 
-    return {register}
+    async function authUser(data){
+        setAuthenticated(true)
+        localStorage.setItem('token', JSON.stringify(data.token))
+
+    }
+
+    function logout(){
+        setAuthenticated(false)
+        localStorage.removeItem("token")
+        api.defaults.headers.Authorization = undefined
+        window.location.reload()
+    }
+
+    return {authenticated, register, logout,login}
 }
  
-export default useAuth;
+export default useAuth
