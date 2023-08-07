c9bb3512031074c7c929bf5d653c8b08c43b8cd3
Wendel Isaac <58012282+wendelisc12@users.noreply.github.com>
2023-08-07 00:38:47 -0300

    Revert "Apicep"

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,23 @@
+import './App.css'
 import Navigation from './components/navigation/Navigation';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Home from './pages/Home/Home';
-import Cadastro from './pages/Cadastro/Cadastro';
-import Login from './pages/Login/login';
-import ForgotPassword from './pages/ForgotPassword/ForgotPassword';
-import { AnimatePresence } from 'framer-motion';
-import PrestadorServicoAnuncio from './pages/prestadorServicoAnuncio/PrestadorServicoAnuncio';
-import { UserProvider } from '../context/userContext';
 
 function App() {
-
-
   return (
     <>
-      <AnimatePresence mode='wait'>
-        <UserProvider>
+
+        <Navigation />
           <BrowserRouter>
 
-            <div>
-              <Route path="/cadastro">
-                <Cadastro />
-              </Route>
+            <Switch>
 
-              <Route path={["/", "/users/:id"]} exact>
-                <Navigation />
-              </Route>
-
-              <Switch>
-
-                <Route path="/login" exact>
-                  <Login />
-                </Route>
-
-                <Route path="/users/:id" exact>
-                  <PrestadorServicoAnuncio />
-                </Route>
-
-                <Route path="/recuperarSenha" exact>
-                  <ForgotPassword />
-                </Route>
-
-                <Route path="/" exact>
-                  <Home />
-                </Route>
-
-              </Switch>
-            </div>
+              <Route path='/'exact>
+                <Home />
+              </Route> 
+                
+            </Switch>
           </BrowserRouter>
-        </UserProvider>
-      </AnimatePresence>
     </>
   )
 }
--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,40 +1,19 @@
 import './navigation.css'
-import {useContext} from 'react'
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-import { Context } from '../../../context/userContext';
 
 const Navigation = () => {
-
-    const{authenticated, logout} = useContext(Context)
-
     return ( 
         <nav id='navegacao'>
             <div className='container d-flex justify-content-between align-items-center h-100'>
                 <div id='logo-box'>
-                    <Link to="/" className='d-flex align-items-center justify-content-center'>
+                    <a href="/" className='d-flex align-items-center justify-content-center'>
                         <img src="/logo.png" alt="" width='60'/>
                         <h4 className='text-light mt-2 p-3 font-weight-bold'>MãoNaObra</h4>
-                    </Link>
+                    </a>
                 </div>
 
                 <div className='d-flex align-items-center justify-content-center'>
-                    {
-                        authenticated ? 
-                        ( 
-                        <div>
-                            <p>logado</p>
-                            <li onClick={logout}>Sair</li>
-                        </div>
-                        ) 
-                        : 
-                        (
-                        <>
-                            <Link to="/login" id='login'>Login</Link>
-                            <Link to="/cadastro/suasInformacoes" id='cadastro'>Criar conta</Link> 
-                        </>
-                        )
-                    }
-                    
+                    <a href="/login" id='login'>Login</a>
+                    <a href="#" id='cadastro'>Criar conta</a>
                 </div>
             </div>
         </nav>
